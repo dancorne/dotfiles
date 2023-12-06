0672c5e6bcdbb22dbd62fdc6a9f36708b743b862
Dan Corne <dan.corne@freeagent.com>
2023-12-06 10:54:38 +0000

    Lots of updates to neovim config
    Hackily moved things from vimrc to lua, switched to lazy.nvim for
    plugin management. More to be moved to Lua though and refactoring to
    come sometime in the future...

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,44 @@
+vim.g.mapleader = " "
+vim.g.inccommand = nosplit
+vim.g.nv_search_paths = { '~/notes' }
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins", {
+  change_detection = {
+    enabled = false
+  }
+})
+require("config")
 
 vim.cmd("source ~/.vimrc")
 
-local nvim_lsp = require('lspconfig')
+-- Autoformat
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "bash-fc.*" },
+  callback = function()
+    vim.api.nvim_command("setfiletype sh")
+  end,
+})
 
--- Use an on_attach function to only map the following keys 
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
+-- Autotest Ruby excercism
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { vim.fn.expand("~") .. "/code/exercism/ruby/**/*.rb" },
+  command = 'setlocal makeprg=ruby\\ %:s?.rb?_test.rb?'
+})
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { vim.fn.expand("~") .. "/code/exercism/ruby/**/*.rb" },
+  command = 'make'
+})
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
@@ -34,32 +59,43 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
   buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
-end
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = {"bashls", "pyls", "solargraph", "gopls"}
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup { on_attach = on_attach }
-end
 
-nvim_lsp.terraformls.setup { on_attach = on_attach,  root_dir = nvim_lsp.util.root_pattern("vars.tf", "variables.tf", ".git") }
+-- Add LSP shortcuts if LSP is activated
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
--- Autocomplete setup
-vim.o.completeopt = "menuone,noselect"
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>F', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
 
   source = {
     path = true;
---
+++ .config/nvim/lua/config.lua
@@ -0,0 +1,44 @@
+vim.opt.number = true
+vim.opt.hidden = true
+vim.opt.cursorline = true
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
+
+vim.cmd.colorscheme("gruvbox8")
+--set syntax
+--vim.opt.t_Co=256 --256 colours
+vim.opt.background = "dark"
+vim.opt.termguicolors = true
+vim.opt.listchars = {
+  eol = "$",
+  tab = ">-",
+  trail = "~",
+  extends = ">",
+  precedes = "<",
+  nbsp = "^",
+}
+--vim.opt.statusline=%<%f\ %h%m%r(%{FugitiveHead()})[%{nvim_treesitter#statusline()}]%=%y[%l,%c][%L][%{&ff}][%p%%]
+vim.opt.signcolumn = "yes"
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  pattern = "*",
+  command = "if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankRegister +' | endif"
+})
+
+-- Open a file on the last line we were at
+-- Doesn't work like this, probably due to the quoting nightmare
+--vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+--  pattern = "*",
+--  command = 'if line("\'\"") > 0 && line("\'\"") <= line("$") | exe "normal! g\'\"" | endif'
+--})
+-- Is this needed any more?
+--autocmd BufEnter * :syntax sync fromstart
+vim.filetype.add {
+  extension = {
+    tf = 'hcl'
+  },
+  filename = {
+    ['terragrunt.hcl'] = 'hcl'
+  }
+}
