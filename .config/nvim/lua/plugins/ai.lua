7742e35983344cae353a034285ef151fabef5fbd
Dan Corne <dan.corne@freeagent.com>
2025-02-24 14:17:27 +0000

    nvim: Lazy load AI tools

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,12 +1,14 @@
 return {
   {
     "github/copilot.vim",
+    cmd = { 'Copilot' },
     init = function()
       vim.g.copilot_filetypes = { ["*"] = false }
     end,
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    cmd = { 'CopilotChatClose', 'CopilotChatDebugInfo', 'CopilotChatFix', 'CopilotChatModels', 'CopilotChatReset', 'CopilotChatStop', 'CopilotChat', 'CopilotChatCommit', 'CopilotChatDocs', 'CopilotChatFixDiagnostic', 'CopilotChatOpen', 'CopilotChatReview', 'CopilotChatTests', 'CopilotChatAgents', 'CopilotChatCommitStaged', 'CopilotChatExplain', 'CopilotChatLoad', 'CopilotChatOptimize', 'CopilotChatSave', 'CopilotChatToggle' },
     dependencies = {
       { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
